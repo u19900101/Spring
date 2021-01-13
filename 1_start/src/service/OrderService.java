99d97522cb66bd6508f1e4ea2b60bbaeae9e3b02
u19900101 <815000342@qq.com>
2021-01-13 15:38:17 +0800

    @Resource 实现属性注入

--- 1_start/src/service/OrderService.java
+++ 1_start/src/service/OrderService.java
@@ -4,11 +4,14 @@ import dao.OrderDao;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * @author lppppp
@@ -20,8 +23,10 @@ import org.springframework.stereotype.Service;
 @Service   //相当于  <bean id="orderService" class="service.OrderService">
 public class OrderService {
     // 加入属性注解
-    @Autowired  // 根据类型进行注入
-    @Qualifier(value = "orderDaoImpl2") // 当有多个实现类时 则按照名称进行注入
+    // @Autowired  // 根据类型进行注入
+    // @Qualifier(value = "orderDaoImpl2") // 当有多个实现类时 则按照名称进行注入
+    // @Resource  // 不带参数  按类型注入
+    @Resource(name = "orderDaoImpl2") // 带参数按名称注入
     private OrderDao orderDao;
     public void add(){
         System.out.println("come into OrderService add ....");
