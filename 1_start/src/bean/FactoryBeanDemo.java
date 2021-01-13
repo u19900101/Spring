672d744af5dfb7e613f83300b3c90ffc3730e180
u19900101 <815000342@qq.com>
2021-01-13 08:52:05 +0800

    设置scope的值为singleton、prototype 单例模式或多例模式

--- 1_start/src/bean/FactoryBeanDemo.java
+++ 1_start/src/bean/FactoryBeanDemo.java
@@ -1,6 +1,7 @@
 package bean;
 
 import bean._1_base.Person;
+import bean._1_base.User;
 import org.junit.Test;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.ApplicationContext;
@@ -32,5 +33,17 @@ public class FactoryBeanDemo implements FactoryBean<Person> {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean4_factory.xml");
         Person factoryBeanDemo = context.getBean("factoryBeanDemo", Person.class);
         System.out.println(factoryBeanDemo);
+    }
+
+    /* 默认为单例模式 */
+    @Test
+    public void T2(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1_base.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println(user); // User@5fdba6f9
+        User user2 = context.getBean("user", User.class);
+        System.out.println(user2);// User@5fdba6f9
+        // 修改bean配置的中的scope为 prototype后 User@10d59286
+
     }
 }
