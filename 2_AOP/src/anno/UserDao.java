789cc33e547b201f3ddaa5e4ea9288ea96846cdb
u19900101 <815000342@qq.com>
2021-01-14 09:00:00 +0800

    相同切入点的抽取

--- 2_AOP/src/anno/UserDao.java
+++ 2_AOP/src/anno/UserDao.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDao {
     public void add(){
+        // int i= 10/0;
         System.out.println("add ....");
     }
 }
--- 2_AOP/src/anno/UserDaoProxy.java
+++ 2_AOP/src/anno/UserDaoProxy.java
@@ -1,7 +1,7 @@
 package anno;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,10 +14,37 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class UserDaoProxy {
+    //相同切入点的抽取
+    @Pointcut(value = "execution(* anno.UserDao.add(..))")
+    public void point(){}
+
     // @Before(value = "execution(* anno.UserDao.add(..))")
-    @Before(value = "execution(* anno.*.*(..))")// 要一一对应，不可跨层
+    // @Before(value = "execution(* anno.*.*(..))")// 要一一对应，不可跨层
+    @Before(value = "point()")
     public void before(){
         System.out.println("before .....");
+    }
+
+    // 最终通知 相当于finally，不管有无异常均会执行
+    @After(value = "execution(* anno.*.*(..))")// 要一一对应，不可跨层
+    public void after(){
+        System.out.println("after .....");
+    }
+    @Around(value = "execution(* anno.*.*(..))")// 要一一对应，不可跨层
+    public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("around before .....");
+        proceedingJoinPoint.proceed();
+        System.out.println("around after .....");
+    }
+    @AfterReturning(value = "execution(* anno.*.*(..))")// 要一一对应，不可跨层
+    public void afterReturning(){
+        System.out.println("AfterReturning .....");
+    }
+
+    // 有异常时会执行
+    @AfterThrowing(value = "execution(* anno.*.*(..))")// 要一一对应，不可跨层
+    public void afterThrowing(){
+        System.out.println("AfterThrowing .....");
     }
 
     @Test
