cfe76beb16868e4d220021bdd5b410fc16dcd3eb
u19900101 <815000342@qq.com>
2021-01-19 15:21:32 +0800

    {param}请求参数的限制

--- 6_SpringMVC/src/main/java/ppppp/controller/HelloWorldController.java
+++ 6_SpringMVC/src/main/java/ppppp/controller/HelloWorldController.java
@@ -2,17 +2,30 @@ package ppppp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author lppppp
  * @create 2021-01-19 10:47
  */
-@RequestMapping("/HelloServlet")
+// @RequestMapping("/HelloServlet")
 @Controller
 public class HelloWorldController {
     // @RequestMapping("hello")  // 写不写 / 都可以
     @RequestMapping("/hello")
     public String Hello(){
+        System.out.println("hello");
+        return "success";
+    }
+
+    @RequestMapping(value = "/h2",method = RequestMethod.POST)
+    public String Hello2(){
+        System.out.println("hello");
+        return "success";
+    }
+    // param
+    @RequestMapping(value = "/h3",params = {"username"})
+    public String Hello3(){
         System.out.println("hello");
         return "success";
     }
--- 6_SpringMVC/src/main/webapp/index.jsp
+++ 6_SpringMVC/src/main/webapp/index.jsp
@@ -1,5 +1,13 @@
+<%@ page contentType="text/html;charset=gbk"%>
 <html>
 <body>
 <h2><a href="hello" />Hello World!</h2>
+
+<h2><a href="h2" />H2</h2>
+
+<h2><a href="h3" />ÇëÇó²ÎÊý</h2>
+<form action="h2" method="post">
+    <input type="submit" value="H2"/>
+</form>
 </body>
 </html>
