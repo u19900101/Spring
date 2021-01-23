57dcf835eaaa516294d2b04ecadf36f8e4ee2aaa
u19900101 <815000342@qq.com>
2021-01-23 15:38:11 +0800

    映射文件的参数传递

--- 7_MyBatis/src/main/java/dao/EmployeeDao.java
+++ 7_MyBatis/src/main/java/dao/EmployeeDao.java
@@ -1,10 +1,15 @@
 package dao;
 
+import org.apache.ibatis.annotations.Param;
 import pojo.Employee;
+
+import java.util.Map;
 
 public interface EmployeeDao{
 
 	Employee getEmpById(Integer id);
+	Employee getEmpByIdAndName(@Param("id") Integer id, @Param("lastName")String lastName);
+	Employee getEmpByMap(Map map);
 	int addEmp(Employee employee);
 	int delEmp(Integer id);
 	int updateEmp(Employee employee);
--- 7_MyBatis/src/test/java/p03_mapper/MapperTest.java
+++ 7_MyBatis/src/test/java/p03_mapper/MapperTest.java
@@ -1,4 +1,4 @@
-package p02_config;
+package p03_mapper;
 
 import dao.EmployeeDao;
 import org.apache.ibatis.io.Resources;
@@ -11,18 +11,20 @@ import pojo.Employee;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
+import java.util.HashMap;
 
 /**
  * @author lppppp
  * @create 2021-01-22 23:07
  */
-public class ConfigTest {
+public class MapperTest {
     public static SqlSessionFactory getSqlSessionFactory() throws IOException {
-        String resource = "p02_config/mybatis-config.xml";
+        String resource = "p03_mapper/mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         return new SqlSessionFactoryBuilder().build(inputStream);
     }
 
+    // 多个参数的获取
     @Test
     public void T() throws IOException {
         SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
@@ -30,8 +32,16 @@ public class ConfigTest {
         try {
             sqlSession = sqlSessionFactory.openSession();
             EmployeeDao employeeDao = sqlSession.getMapper(EmployeeDao.class);
-            Employee kk = employeeDao.getEmpById(1);
-            System.out.println(kk);
+            Employee kk = employeeDao.getEmpByIdAndName(11,"kk");
+            HashMap <String,Object> map = new HashMap<>();
+            map.put("id", 11);
+            map.put("lastName", "kk");
+            // 动态的修改表名
+            // map.put("tableName", "t_employee");
+            map.put("tableName", "t_employee_copy");
+            Employee empByMap = employeeDao.getEmpByMap(map);
+            System.out.println(empByMap);
+            // System.out.println(kk);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
